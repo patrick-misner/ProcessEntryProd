5df8d66636a38e042d634d3c82f7f5ad1dc9a11d
Patrick Misner <misner.patrick@gmail.com>
2023-01-23 05:35:56 -0700

    commit

--- ProcessEntryProd/Program.cs
+++ ProcessEntryProd/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseMySql(builder.Configuration.GetConnectionString("Default"), new MySqlServerVersion( new Version(8, 0, 30))));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
